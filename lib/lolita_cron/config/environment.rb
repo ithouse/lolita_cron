0abc6b5461c78e47f404e1fe265b5df619c2d1f7
Gatis Tomsons <gatis@ithouse.lv>
2011-01-28 15:08:24 +0200

    Now daemon runs with DAEMON_ENV as suffix, like lolita_cron_roduction or lolita_cron_staging allowing to run multiple instances on the same server

--- lib/lolita_cron/config/environment.rb
+++ lib/lolita_cron/config/environment.rb
@@ -17,15 +17,15 @@ require File.join(File.dirname(__FILE__), 'boot')
 require 'rubygems'
 
 gem 'daemon-kit', '~> 0.1.8.1'
-gem 'eventmachine'
+gem 'eventmachine', '~> 0.12.10'
 gem 'rufus-scheduler', '~> 2.0.8'
 
 require('ruby-debug') if RAILS_ENV == 'development'
 
 DaemonKit::Initializer.run do |config|
-  config.daemon_name = "lolita_cron"
+  config.daemon_name = "lolita_cron_#{DaemonKit.env}"
   config.pid_file = "#{RAILS_ROOT}/tmp/pids/lolita_cron_#{DaemonKit.env}.pid"
-  config.log_path = "#{RAILS_ROOT}/log/lolita_cron_#{RAILS_ENV}.log"
+  config.log_path = "#{RAILS_ROOT}/log/lolita_cron_#{DaemonKit.env}.log"
   config.force_kill_wait = 30
   Rails.logger.level = Logger::Severity::UNKNOWN
 end
--- lib/lolita_cron/config/pre-daemonize/cron.rb
+++ lib/lolita_cron/config/pre-daemonize/cron.rb
@@ -9,3 +9,14 @@ if Rufus::Scheduler::VERSION < "2.0.0"
   $stderr.puts "Requires rufus-scheduler-2.0.0 or later"
   exit 1
 end
+
+# Monkeypatch - to run daemons for many environments on the same server
+module DaemonKit
+  class Initializer
+    def include_core_lib
+      if File.exists?( core_lib = File.join( DAEMON_ROOT, 'lib', 'lolita_cron.rb' ) )
+        require core_lib
+      end
+    end
+  end
+end
